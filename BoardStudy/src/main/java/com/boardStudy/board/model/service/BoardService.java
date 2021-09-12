918453bae0de8f0ee524d821baa30bdcf0eea885
choisk94 <84427722+choisk94@users.noreply.github.com>
2021-09-12 23:34:45 +0900

    Etc : 서비스에 기능 구분을 위한 주석 추가
    Author : seokyung choi
    
    - add comment on BoardService.java

--- BoardStudy/src/main/java/com/boardStudy/board/model/service/BoardService.java
+++ BoardStudy/src/main/java/com/boardStudy/board/model/service/BoardService.java
@@ -6,14 +6,19 @@ import com.boardStudy.board.model.vo.Board;
 
 public interface BoardService {
 	
+	// 게시글 작성
 	void insertBoard(Board board);
 	
+	// 게시글 수정
 	void updateBoard(Board board);
 	
+	// 게시글 삭제
 	void deleteBoard(Board board);
 	
+	// 게시글 조회
 	Board getBoard(Board board);
 	
+	// 게시글 목록 조회
 	Page<Board> getBoardList(Board board);
 
 }
